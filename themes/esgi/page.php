1e723e4d5fea0c959c548a4128b89b2158a5e577
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-06-09 16:25:35 +0200

    Responsive design du widget

--- themes/esgi/page.php
+++ themes/esgi/page.php
@@ -1,20 +1,12 @@
 <?php
 get_header();
 
-
-if(is_active_sidebar('sidebar')){
-	echo "<div id='sidebar'>";
-		echo "<ul>";
-			dynamic_sidebar('sidebar');	
-		echo "</ul>";
-	echo "</div>";
-}
 if(have_posts()){
 
 	while(have_posts()):
 		the_post();
 		?>
-		<article class="post">
+		<article class="post contenu">
 			<h1><?php the_title(); ?></h1>
 			<div><?php the_content(); ?> </div>
 			<ol>
@@ -30,6 +22,14 @@ else{
 	<?php _e('Sorry, no posts matched your criteria.');?>
 	</p>
 	<?php
+}
+
+if(is_active_sidebar('sidebar')){
+	echo "<div id='sidebar'>";
+		echo "<ul>";
+			dynamic_sidebar('sidebar');	
+		echo "</ul>";
+	echo "</div>";
 }
 
 get_footer();
