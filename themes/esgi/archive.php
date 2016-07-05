06b3e14fcb0b7b9776dcc357e666be4376572817
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-07-05 14:06:05 +0200

    Mise en place du modèle Archive pour afficher la liste de tous les articles

--- themes/esgi/archive.php
+++ themes/esgi/archive.php
@@ -1,35 +1,58 @@
 <?php
+/*
+Template Name: Archives chronologique
+*/
+
 get_header();
+?>
 
-echo '<h1>CECI EST UNE ARCHIVE 2</h1>';
-if(have_posts()){
-	while(have_posts()):
-		the_post();
+<div><?php the_content(); ?></div>
+
+<?php
+
+$previous_year = $year = 0;
+$previous_month = $month = 0;
+$ul_open = false;
+
+$myposts = get_posts('numberposts=-1&orderby=post_date&order=DESC');
+
+foreach($myposts as $post){
+
+	setup_postdata($post);
+
+	$year = mysql2date('Y', $post->post_date);
+	$month = mysql2date('n', $post->post_date);
+	$day = mysql2date('j', $post->post_date);
+
+	if($year != $previous_year || $month != $previous_month){
+		if($ul_open == true){
+			echo '</ul>';
+		}
 		?>
-		<article class="post">
-			<h1><?php the_title(); ?></h1>
-			<div><?php the_content(); ?> </div>
-			<?php
-			comments_template();
-			comment_form();
-			?>
-			<ol>
-				<?php wp_list_comments(); ?>
-			</ol>
-		</article>
-		<?php
-	endwhile;
-}
-else{
-	?>
-	<p>
-	<?php _e('Sorry, no posts matched your criteria.');?>
-	</p>
-	<?php
+
+		<h3><?php the_time('F Y'); ?></h3>
+
+		<ul>
+			<?php $ul_open = true;
+	}
+	$previous_year = $year; 
+	$previous_month = $month; ?>
+
+	<li>
+		<span><?php the_time('j F'); ?> - </span>
+		<span><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span>
+	</li>
+<?php 
 }
 
-if(is_active_sidebar('sidebar-1')){
-	dynamic_sidebar('sidebar-1');
+echo '</ul>';
+
+if(is_active_sidebar('sidebar')){
+	echo "<div id='sidebar'>";
+		echo "<ul>";
+			dynamic_sidebar('sidebar');	
+		echo "</ul>";
+	echo "</div>";
 }
 
 get_footer();
--- themes/esgi/category.php
+++ themes/esgi/category.php
@@ -1,21 +1,14 @@
 <?php
 get_header();
 
-echo '<h1>CECI EST UNE LISTE DES CATEGORIES</h1>';
 if(have_posts()){
+
 	while(have_posts()):
 		the_post();
 		?>
-		<article class="post">
+		<article class="post contenu">
 			<h1><?php the_title(); ?></h1>
-			<div><?php the_content(); ?> </div>
-			<?php
-			comments_template();
-			comment_form();
-			?>
-			<ol>
-				<?php wp_list_comments(); ?>
-			</ol>
+			<div class="text_single"><?php the_content(); ?></div>
 		</article>
 		<?php
 	endwhile;
@@ -28,8 +21,12 @@ else{
 	<?php
 }
 
-if(is_active_sidebar('sidebar-1')){
-	dynamic_sidebar('sidebar-1');
+if(is_active_sidebar('sidebar')){
+	echo "<div id='sidebar'>";
+		echo "<ul>";
+			dynamic_sidebar('sidebar');	
+		echo "</ul>";
+	echo "</div>";
 }
 
 get_footer();
